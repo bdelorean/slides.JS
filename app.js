4ef70c91a2828a331466b586dc72b5e623dcb80a
bdelorean <9021196@student.zadkine.nl>
2024-07-14 17:34:44 +0200

    slides.default

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-// Selectăm toate elementele cu clasa 'slide' și le stocăm în variabila 'slides'
-const slides = document.querySelectorAll(".slide");
-// Iterăm prin fiecare element 'slide' din 'slides'
-for (const slide of slides) {
-  slide.addEventListener("click", () => {
-    // Când un 'slide' este click-uit, apelăm funcția 'clearActiveSlides'
-    clearActiveSlides();
-    // Adăugăm clasa 'active' la slide-ul care a fost click-uit
-    slide.classList.add("active");
-  });
-}
+function initSlides(active = 3) { // slide-ul cu indexul 3 v-a avea din default clasa active 
+  // Selectăm toate elementele cu clasa 'slide' și le stocăm în variabila 'slides'
+  const slides = document.querySelectorAll(".slide");
 
-// Funcție pentru a elimina clasa 'active' de la toate slide-urile
-function clearActiveSlides() {
+  slides[active].classList.add('active'); //Accesăm elementul din slides aflat la poziția specificată de variabila active.
+
   // Iterăm prin fiecare element 'slide' din 'slides'
-  slides.forEach((slide) => {
-    // Eliminăm clasa 'active' de la elementul curent
-    slide.classList.remove("active");
-  });
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      // Când un 'slide' este click-uit, apelăm funcția 'clearActiveSlides'
+      clearActiveSlides();
+      // Adăugăm clasa 'active' la slide-ul care a fost click-uit
+      slide.classList.add("active");
+    });
+  }
+
+  // Funcție pentru a elimina clasa 'active' de la toate slide-urile
+  function clearActiveSlides() {
+    // Iterăm prin fiecare element 'slide' din 'slides'
+    slides.forEach((slide) => {
+      // Eliminăm clasa 'active' de la elementul curent
+      slide.classList.remove("active");
+    });
+  }
 }
+initSlides(); // vrei sa schimbi imaginea activa din default doar dai alt parametru aici in paranteze in loc de 3 pui 2 de exemplu
